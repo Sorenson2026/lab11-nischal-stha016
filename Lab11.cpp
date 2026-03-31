4a530d41222e07be6131620548a181e8a02acddf
nischal.shrestha <nischal.shrestha@ENGRDHCP5S3>
2026-03-31 15:04:40 -0600

    Program can now find lowest and Highest

--- Lab11.cpp
+++ Lab11.cpp
@@ -10,11 +10,13 @@ using namespace std;
 
 const int SIZE = 100;
 
+// Function prototypes
 int readFile(const string& filename, double numbers[], int& count);
 double findLowest(double numbers[], int count);
-
+double findHighest(double numbers[], int count);
 void analyzeArray(double numbers[], int count);
 
+// Main function
 int main()
 {
 	double numbers[SIZE];
@@ -27,6 +29,7 @@ int main()
 	return 0;
 }
 
+// Function to read numbers from a file into an array
 int readFile(const string& filename, double numbers[], int& count)
 {
 	ifstream file(filename);
@@ -44,6 +47,7 @@ int readFile(const string& filename, double numbers[], int& count)
 	return count;
 }
 
+// Function to find the lowest number in the array
 double findLowest(double numbers[], int count)
 {
 	double lowest = numbers[0];
@@ -57,19 +61,34 @@ double findLowest(double numbers[], int count)
 	return lowest;
 }
 
+// Function to find the highest number in the array
+double findHighest(double numbers[], int count)
+{
+	double highest = numbers[0];
+	for (int i = 1; i < count; i++)
+	{
+		if (numbers[i] > highest)
+		{
+			highest = numbers[i];
+		}
+	}
+	return highest;
+}
+
 void analyzeArray(double numbers[], int count)
 {
 	double lowest = findLowest(numbers, count);
-	/*double highest = findHighest(numbers, count);
-	double total = calculateTotal(numbers, count);
+	double highest = findHighest(numbers, count);
+	/*double total = calculateTotal(numbers, count);
 	double average = calculateAverage(numbers, count);
 	double stddev = calculateStandardDeviation(numbers, count, average);
 
 	
-	cout << "Highest: " << highest << endl;
+	
 	cout << "Total: " << total << endl;
 	cout << "Average: " << average << endl;
 	cout << "Standard Deviation: " << stddev << endl; */
 	cout << "Lowest: " << lowest << endl;
+	cout << "Highest: " << highest << endl;
 }
 
